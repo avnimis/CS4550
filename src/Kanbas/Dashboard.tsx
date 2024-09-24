7d3c136d03375621e92a1967550d5c7b46f78c04
Avni Mishra <avni.mishra0409@gmail.com>
2024-09-24 19:56:55 -0400

    Added Dashboard Courses

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -7,11 +7,11 @@ export default function Dashboard() {
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
           <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
+            to="/Kanbas/Courses/1234/Home">
             <img src="/images/reactjs.jpg" width={200} />
             <div>
               <h5>
-                 CS1234 React JS
+                CS1234 React JS
               </h5>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
@@ -20,8 +20,103 @@ export default function Dashboard() {
             </div>
           </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+
+        <div className="wd-dashboard-course">
+          <Link className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home">
+            <img src="/images/Sheep.jpg" width={200} />
+            <div>
+              <h5>
+                Sheep Shearing and Maintenace  
+              </h5>
+              <p className="wd-dashboard-course-title">
+                Barn handling development 
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course"> 
+          <Link className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home">
+            <img src="/images/chicken.jpg" width={200} />
+            <div>
+              <h5>
+                Coop preperation course 
+              </h5>
+              <p className="wd-dashboard-course-title">
+                CS1210 
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course"> 
+          <Link className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home">
+            <img src="/images/mines.jpg" width={200} />
+            <div>
+              <h5>
+                Rocks and Minerals
+              </h5>
+              <p className="wd-dashboard-course-title">
+                Enviornmental Sciences 
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course"> 
+          <Link className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home">
+            <img src="/images/fish.jpg" width={200} />
+            <div>
+              <h5>
+                Fishing 101
+              </h5>
+              <p className="wd-dashboard-course-title">
+                Marine Biology
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course"> 
+          <Link className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home">
+            <img src="/images/desert.jpg" width={200} />
+            <div>
+              <h5>
+                Climate Change in humid climates
+              </h5>
+              <p className="wd-dashboard-course-title">
+                Enviornmental Sciences
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course"> 
+          <Link className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home">
+            <img src="/images/dinosaurs.jpg" width={200} />
+            <div>
+              <h5>
+                Raising dinosaurs
+              </h5>
+              <p className="wd-dashboard-course-title">
+                Paleontology
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
       </div>
     </div>
   );
